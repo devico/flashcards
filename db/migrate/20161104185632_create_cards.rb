9d4b68818ced5449eea6c313e3b8f2a0a002396c
devico <clamdm@mail.ru>
2016-11-07 00:24:10 +0200

    rails5

--- db/migrate/20161104185632_create_cards.rb
+++
@@ -1,9 +0,0 @@
-class CreateCards < ActiveRecord::Migration
-  def change
-    create_table :cards do |t|
-      t.string :original_text
-      t.string :translated_text
-      t.date :review_date
-    end
-  end
-end
