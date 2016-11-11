7e536cd39e7397f65f11b9715eddb42cf184b140
devico <clamdm@mail.ru>
2016-11-11 19:19:40 +0200

    tests redone its work

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,9 +1,15 @@
+require 'rails_helper'
 describe Card do
 
   describe '#check_original_and_translated_text' do
     subject { Card.new(card_params) }
-    let(:card_params) { { original_text: "Дом", translated_text: " дом" } }
-    it { expect(subject.save).to be_falsey }
-   end
 
+    it "is valid when the texts are not the same" do
+      expect(Card.new(original_text: "House", translated_text: "дом")).to be_valid
+    end
+
+    it "is not valid when the texts are identical" do
+     expect(Card.new(original_text: "Дом", translated_text: " дом")).to_not be_valid
+    end
+   end
 end
