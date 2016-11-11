858c35ae7d7b45bc2a4093bf3174871b13c64e2f
devico <clamdm@mail.ru>
2016-11-11 23:16:50 +0200

    made test for model

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 describe Card do
 
   describe '#check_original_and_translated_text' do
-    subject { Card.new(card_params) }
 
     it "is valid when the texts are not the same" do
       expect(Card.new(original_text: "House", translated_text: "дом")).to be_valid
@@ -11,5 +10,15 @@ describe Card do
     it "is not valid when the texts are identical" do
      expect(Card.new(original_text: "Дом", translated_text: " дом")).to_not be_valid
     end
+
+   end
+
+    describe '#set_review_date' do
+
+      it "when original text equal translated text" do
+        card = Card.new(original_text: "Next", translated_text: "следующий")
+        expect(card.set_review_date).to eq(Date.today + 3.days)
+      end
+
    end
 end
