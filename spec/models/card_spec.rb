181307eeb1de642a0d5c43dee51cf7ffd1be7358
devico <clamdm@mail.ru>
2016-11-12 12:20:00 +0200

    add test interactor with errors

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -10,6 +10,24 @@ describe Card do
     it "is not valid when the texts are identical" do
       expect(Card.new(original_text: "Дом", translated_text: " дом")).to_not be_valid
     end
+
+    it "is not valid when original_text is nil" do
+      card = Card.new(original_text: "House", translated_text: "дом")
+      card.original_text = nil
+      expect(card.errors[:original_text]).to include("can't be blank")
+    end
+
+    it "is not valid when original_text is nil" do
+      card = Card.new(original_text: "House", translated_text: "дом")
+      card.translated_text = nil
+      expect(card.errors[:translated_text]).to include("can't be blank")
+    end
+
+    it "is not valid when original_text is nil" do
+      card = Card.new(original_text: "дом", translated_text: "дом")
+      expect(card.errors.values).to include("original and translated text can’t be same")
+    end
+
    end
 
     describe '#set_review_date' do
