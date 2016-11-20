928a73b45204334360b6025faeaaad3393a75b52
devico <clamdm@mail.ru>
2016-11-20 11:38:24 +0200

    cards controller tests #create and #show with failures

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -2,17 +2,35 @@ require 'rails_helper'
 
 RSpec.describe CardsController, :type => :controller do
 
-  before do
-    user = FactoryGirl.create(:user)
-  end
-
   describe "#create" do
 
+    before(:each) do
+      current_user = FactoryGirl.create(:user)
+    end
+
     it "redirects to the card's page after create card" do
-      post :create, card: { original_text: "Miss", translated_text: "Девушка", user_id: 9}
+      post :create, card: FactoryGirl.attributes_for(:card)
+      merge({user_id: current_user.id})
       expect(response).to redirect_to(assigns(:card))
     end
 
+    it "renders #new form when validates fail" do
+      post :create, card: FactoryGirl.attributes_for(:card)
+      merge({original_text: ""})
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "#show" do
+
+    before do
+      card = FactoryGirl.create(:card)
+    end
+
+    it "show card page" do
+      get :show, id: card.id
+      expect(response).to render_template("show")
+    end
   end
 end
 
