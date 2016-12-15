0ca6a936db886dca9c359b8ab663e4688de1d192
devico <clamdm@mail.ru>
2016-12-15 21:00:08 +0200

    refactoring code interactor check_card

--- app/interactors/check_card.rb
+++ app/interactors/check_card.rb
@@ -2,24 +2,32 @@ class CheckCard
   include Interactor
 
   def call
-    card = Card.find(context.card_id)
-    if card.original_text == context.user_text
-      card.success_counter += 1
-      card.fail_counter = 0
-      card.save
-      date_for_review = selection_delay(card.success_counter)
-      card.update(review_date: date_for_review)
-      context.message = "Правильно"
-      context.card = card
+    @card = Card.find(context.card_id)
+    if @card.original_text == context.user_text
+      correct_answer  
     else
-      card.fail_counter += 1
-      card.save
-      if card.success_counter >= 3 && card.fail_counter == 3
-        card.update(review_date: 12.hours.since, success_counter: 0)
-      end
-      context.card = card
-      context.message = "Не правильно"
+      incorrect_answer
     end
+  end
+
+  def correct_answer
+    @card.success_counter += 1
+    @card.save
+    @card.fail_counter = 0
+    date_for_review = selection_delay(@card.success_counter)
+    @card.update(review_date: date_for_review)
+    context.message = "Правильно"
+    context.card = @card
+  end
+
+  def incorrect_answer
+    @card.fail_counter += 1
+    @card.save
+    if @card.success_counter >= 3 && @card.fail_counter == 3
+      @card.update(review_date: 12.hours.since, success_counter: 0)
+    end
+    context.message = "Не правильно"
+    context.card = @card
   end
 
   def selection_delay(counter)
@@ -39,4 +47,3 @@ class CheckCard
     end 
   end
 end
-
