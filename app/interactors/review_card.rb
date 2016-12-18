c5fed851bdf9b1e6dfefe0105ffb8ee669043613
devico <clamdm@mail.ru>
2016-12-18 11:24:31 +0200

    refactor code review_card

--- app/interactors/review_card.rb
+++ app/interactors/review_card.rb
@@ -2,17 +2,24 @@ class ReviewCard
   include Interactor
 
   def call
-    context.card = 
-      if context.user
-        if context.user.current_deck
-          context.user.current_deck.cards
-                 .where("review_date <= ?", Time.current).order("RANDOM()").first
-        else
-          context.user.cards
-                 .where("review_date <= ?", Time.current).order("RANDOM()").first
-        end
-      else
-        Card.where("review_date <= ?", Time.current).order("RANDOM()").first
-      end
+    context.card = if context.user
+                     cards_current_user
+                   else
+                     all_cards
+                   end
+  end
+
+  def cards_current_user
+    if context.user.current_deck
+      context.user.current_deck.cards
+             .where("review_date <= ?", Time.current).order("RANDOM()").first
+    else
+      context.user.cards
+             .where("review_date <= ?", Time.current).order("RANDOM()").first
+    end
+  end
+
+  def all_cards
+    Card.where("review_date <= ?", Time.current).order("RANDOM()").first
   end
 end
