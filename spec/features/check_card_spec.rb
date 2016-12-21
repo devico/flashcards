8a9d06648a92e25c00dab3c3c4080fe3d8cdddf3
devico <clamdm@mail.ru>
2016-12-21 19:40:07 +0200

    add current user to feature spec

--- spec/features/check_card_spec.rb
+++ spec/features/check_card_spec.rb
@@ -4,7 +4,9 @@ RSpec.feature "Card check", :type => :feature do
 
   before do
     @user = FactoryGirl.create(:user)
-    @deck = FactoryGirl.create(:deck)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+    @deck = FactoryGirl.create(:deck, user_id: @user.id)
     @card = Card.create(original_text: 'clean',
                         translated_text: 'чистый',
                         image: 'minion.png',
@@ -16,7 +18,7 @@ RSpec.feature "Card check", :type => :feature do
   scenario "successfully checks card" do
     visit root_path
 
-    fill_in 'Enter original text', with: 'clean'
+    fill_in 'Enter original text', with: @card.original_text
     click_button 'Save Check'
 
     expect(page).to have_text("Правильно")
