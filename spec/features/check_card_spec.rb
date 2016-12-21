a464528a8a7d7deb05c82f24da68e41f762ddb46
devico <clamdm@mail.ru>
2016-12-21 19:55:57 +0200

    change code check_card

--- spec/features/check_card_spec.rb
+++ spec/features/check_card_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Card check", :type => :feature do
 
-  before do
+  scenario "successfully checks card" do
     @user = FactoryGirl.create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
@@ -13,9 +13,7 @@ RSpec.feature "Card check", :type => :feature do
                         user_id: @user.id,
                         deck_id: @deck.id,  
                         review_date: Date.today)
-  end
 
-  scenario "successfully checks card" do
     visit root_path
 
     fill_in 'Enter original text', with: @card.original_text
