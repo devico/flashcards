0c33d6b8754058d8266109ac5297f77bbeaf861c
devico <clamdm@mail.ru>
2016-12-21 20:43:42 +0200

    add signup to feature spec

--- spec/features/check_card_spec.rb
+++ spec/features/check_card_spec.rb
@@ -2,10 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "Card check", :type => :feature do
 
-  scenario "successfully checks card" do
+  before(:each) do
     @user = FactoryGirl.create(:user)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-
     @deck = FactoryGirl.create(:deck, user_id: @user.id)
     @card = Card.create(original_text: 'clean',
                         translated_text: 'чистый',
@@ -13,12 +11,23 @@ RSpec.feature "Card check", :type => :feature do
                         user_id: @user.id,
                         deck_id: @deck.id,  
                         review_date: Date.today)
+    sign_up(@user.username, @user.password)
+  end
 
+  scenario "successfully checks card" do
     visit root_path
 
     fill_in 'Enter original text', with: @card.original_text
     click_button 'Save Check'
 
     expect(page).to have_text("Правильно")
+  end
+
+  def sign_up(name, password)
+    visit '/users/new'
+    fill_in 'Username', with: name
+    fill_in 'Password', with: password
+    fill_in 'Confirm password', with: password
+    click_button 'Create User'
   end
 end
