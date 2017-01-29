69233980b49df0f0545173c8cdbd4dc170f0afa8
devico <clamdm@mail.ru>
2017-01-30 01:42:54 +0200

    add set shell

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
+set :default_shell, '/bin/bash -l'
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
