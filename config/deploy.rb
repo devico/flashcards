75b9aa95786f094a574ff257dec2aae5d9741dfc
devico <clamdm@mail.ru>
2017-01-30 10:26:03 +0200

    unset deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,6 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :shell, '/usr/bin/bash -l'
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
